bdb977bbb551bc284446440932f71b3162b079d5
Sam Carlton <sam@sam.lc>
2019-05-11 11:25:16 -0500

    Add icons

---
+++ resources/assets/js/helpers/faIcons.js
@@ -0,0 +1,68 @@
+export default [
+    {
+      value: 'fas fa-camera'
+    },
+    {
+      value: 'fab fa-firefox'
+    },
+    {
+      value: 'fas fa-volume-off'
+    },
+    {
+      value: 'fas fa-sync'
+    },
+    {
+      value: 'fas fa-redo'
+    },
+    {
+      value: 'fas fa-plane-departure'
+    },
+    {
+      value: 'fab fa-mandalorian'
+    },
+    {
+      value: 'fas fa-magnet'
+    },
+    {
+      value: 'fas fa-check-double'
+    },
+    {
+      value: 'fab fa-bitbucket'
+    },
+    {
+      value: 'fab fa-napster'
+    },
+    {
+      value: 'fab fa-angular'
+    },
+    {
+      value: 'fab fa-creative-commons'
+    },
+    {
+      value: 'fab fa-empire'
+    },
+    {
+      value: 'fab fa-first-order'
+    },
+    {
+      value: 'fab fa-galactic-republic'
+    },
+    {
+      value: 'fab fa-nintendo-switch'
+    },
+    {
+      value: 'fas fa-adjust'
+    },
+    {
+      value: 'fas fa-angle-double-right'
+    },
+    {
+      value: 'fas fa-apple-alt'
+    },
+    {
+      value: 'fas fa-ballot'
+    },
+    {
+      value: 'fas fa-bat'
+    },
+  ]
