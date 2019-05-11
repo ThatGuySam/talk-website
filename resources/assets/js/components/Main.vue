cea03cce61c75c9665276df6b6bf8ded1aa2891d
Sam Carlton <sam@sam.lc>
2019-05-11 13:24:52 -0500

    Add string seed input

--- resources/assets/js/components/Main.vue
+++ resources/assets/js/components/Main.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="container">
-    <div class="row align-items-center" style="min-height: 100vh;">
-      <div class="col-12 w-100">
-        <Carousel />
+  <div class="container-fluid px-0">
+    <div class="container">
+      <div class="row align-items-center" style="min-height: 100vh;">
+        <div class="col-12 w-100">
+          <div class="hero-wrapper d-flex align-items-center p-5" style="min-height: 450px;">
+            <div class="string-seed-input hero-content display-3 font-weight-bold" contenteditable="true" placeholder="Type anything"></div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
