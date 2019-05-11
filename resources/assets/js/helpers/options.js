bbdeb8e870be30460b53c3bb806834fe60307049
Sam Carlton <sam@sam.lc>
2019-05-11 16:02:46 -0500

    Add options

---
+++ resources/assets/js/helpers/options.js
@@ -0,0 +1,57 @@
+
+export const imageIds = [
+  'BE9AifuJfD4',
+  '0mqJjxvMJSs',
+  '2AoJ9TnuDIg',
+  'prCTBqPJrtg',
+  'jxVBTMddTTE',
+  'oZTfS0UhSzM',
+  'Y84-eo8drzk',
+  'hJLJX44zcnM',
+  'lySaP-0Gfm8',
+  'wIIgken3QuM',
+  'eMayHt-uiBU',
+  'jZfFJnwaLG4',
+  'f9lF0bn0UnQ',
+  'F8NIC0FAEgc',
+  'XgSawcCfrbM',
+  'Ec6KP5YdSBU',
+  'TZOHvVj-Jdo',
+  'nZ1Jrugj5I4',
+  '04RhrsalOmU',
+  'emxAxutgt-A',
+  'Gruxs0ZQw7E',
+  'kiotxkttUj0',
+  'MvftAbKK9Ek',
+  'U8opY9umz8Y',
+  'DUC0SNRxQIU',
+  'DjlKxYFJlTc',
+  '-sw_uFvKBkk',
+]
+
+
+export const fonts = [
+  'titling-gothic-fb-extended, sans-serif',
+  'titling-gothic-fb-extended, sans-serif'
+]
+
+// export const patterns = [
+//   'titling-gothic-fb-extended, sans-serif',
+//   'titling-gothic-fb-extended, sans-serif'
+// ]
+
+export const animations = [
+  'fadeIn',
+  'fadeInDown',
+  'fadeInUp',
+  'fadeInDownBig',
+  'fadeInUpBig',
+  'zoomIn',
+  'flipInX'
+]
+
+export const listStyle = [
+  'masonry',
+  'grid',
+  'stack'
+]
