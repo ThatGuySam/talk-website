209fba7def3a0327fa3f322437a5e1d85d0f94b0
Sam Carlton <sam@sam.lc>
2019-05-11 07:34:03 -0500

    Add colors

---
+++ resources/assets/js/helpers/colors.js
@@ -0,0 +1,46 @@
+export default [
+    {
+      name: 'yellow',
+      value: '#f0c30b'
+    },
+    {
+      name: 'orange',
+      value: '#f35908'
+    },
+    {
+      name: 'lightOrange',
+      value: '#ff7c00'
+    },
+    {
+      name: 'darkOrange',
+      value: '#ff2a00'
+    },
+    {
+      name: 'red',
+      value: '#ff0000'
+    },
+    {
+      name: 'darkRed',
+      value: '#820114'
+    },
+    {
+      name: 'lightBlue',
+      value: '#0eabb9'
+    },
+    {
+      name: 'blue',
+      value: '#006cc9'
+    },
+    {
+      name: 'lightPink',
+      value: '#ff66b2'
+    },
+    {
+      name: 'pink',
+      value: '#e33683'
+    },
+    {
+      name: 'purple',
+      value: '#251333'
+    }
+  ]
